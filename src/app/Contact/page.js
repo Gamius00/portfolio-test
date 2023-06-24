06e0fd775cc9f5aac0f5be0ce3001f3936cc5617
Mona Lisa <GescheaftlichGamius@gmx.de>
2023-06-24 22:01:01 +0200

    Changes/ Fixes

--- src/app/Contact/page.js
+++ src/app/Contact/page.js
@@ -1,9 +1,7 @@
-"use client"
 
 import React from 'react'
 import Navbar from '../Navbar'
 import style from "./contact.module.css"
-import { Content } from 'next/font/google'
 
 export default function page() {
 
@@ -12,7 +10,7 @@ export default function page() {
         <Navbar/>
 
       <div className={style.contentflexbox}>
-        <div onMouseOver={() => {}} className={style.contactfield}>
+        <div className={style.contactfield}>
           <p className={style.headtext}>Test</p>
           <p className={style.contacttext}>Test2</p>
         </div>
--- src/app/Navbar.js
+++ src/app/Navbar.js
@@ -1,4 +1,4 @@
-"use client"
+
 
 import React from 'react'
 import style from "./navbar.module.css"
